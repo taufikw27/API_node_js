1792f550fe3166e0c8411776a503028d23386ea3
taufikw27 <taufikwitri@gmail.com>
2022-11-07 10:38:10 +0800

    second

--- app.js
+++ app.js
@@ -40,8 +40,68 @@ app.get('/api/mahasiswa', (req, res)=>{
         res.status(200).json({success: true, data:rows});
     })
 })
+//update data
+app.put("/api/mahasiswa/:id", (req, res) => {
+    // buat variabel penampung data dan query sql
+    const data = { ...req.body };
+    const querySearch = "SELECT * FROM mahasiswa WHERE id_mhs = ?";
+    const queryUpdate = "UPDATE mahasiswa SET ? WHERE id_mhs = ?";
+  
+    // jalankan query untuk melakukan pencarian data
+    koneksi.query(querySearch, req.params.id, (err, rows, field) => {
+      // error handling
+      if (err) {
+        return res.status(500).json({ message: "Ada kesalahan", error: err });
+      }
+      // jika id yang dimasukkan sesuai dengan data yang ada di db
+      if (rows.length) {
+        // jalankan query update
+        koneksi.query(queryUpdate, [data, req.params.id], (err, rows, field) => {
+          // error handling
+          if (err) {
+            return res.status(500).json({ message: "Ada kesalahan", error: err });
+          }
+  
+          // jika update berhasil
+          res.status(200).json({ success: true, message: "Berhasil update data!" });
+        });
+      } else {
+        return res.status(404).json({ message: "Data tidak ditemukan!", success: false });
+      }
+    });
+  });
+// delete data
+app.delete("/api/mahasiswa/:id", (req, res) => {
+    // buat query sql untuk mencari data dan hapus
+    const querySearch = "SELECT * FROM mahasiswa WHERE id_mhs = ?";
+    const queryDelete = "DELETE FROM mahasiswa WHERE id_mhs = ?";
+  
+    // jalankan query untuk melakukan pencarian data
+    koneksi.query(querySearch, req.params.id, (err, rows, field) => {
+      // error handling
+      if (err) {
+        return res.status(500).json({ message: "Ada kesalahan", error: err });
+      }
+  
+      // jika id yang dimasukkan sesuai dengan data yang ada di db
+      if (rows.length) {
+        // jalankan query delete
+        koneksi.query(queryDelete, req.params.id, (err, rows, field) => {
+          // error handling
+          if (err) {
+            return res.status(500).json({ message: "Ada kesalahan", error: err });
+          }
+  
+          // jika delete berhasil
+          res.status(200).json({ success: true, message: "Berhasil hapus data!" });
+        });
+      } else {
+        return res.status(404).json({ message: "Data tidak ditemukan!", success: false });
+      }
+    });
+});
 
 //log server
 app.listen(PORT, () => {
-    console.log('server running at port:',PORT);
+    console.log('server running at port: ',PORT);
 })
